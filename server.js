e368173bff8d3d7d4d2fd26fd74ec4a632986d01
Asif <asif_ruhi@yahoo.com>
2018-04-07 12:00:52 -0700

    adding express server for production build use

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-import express from 'express';
-import proxy from 'express-http-proxy';
+const express = require('express');
+const proxy = require('express-http-proxy');
+const path = require('path');
 
 const app = express();
 
-app.use('/products', proxy('www.westelm.com'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/services/*', proxy('https://www.westelm.com/services'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
